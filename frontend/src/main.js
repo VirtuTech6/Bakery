44287369320aa961e3833daec39a18b17d0b7149
Maxime Revaux <maxime.revaux.etu@univ-lille.fr>
2025-02-04 19:11:22 +0100

    projet clean

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios';
 
-createApp(App).mount('#app')
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api'; // Lien vers Symfony API
+createApp(App).mount('#app')
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,7 @@
-module.exports = {
-  transpileDependencies: true,
-  devServer: {
-    port: 8080,
-  },
-};
-
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 module.exports = {
   devServer: {
     proxy: 'http://127.0.0.1:8000'
